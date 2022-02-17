ac1d83fd78630fd847f23c2b1becb531e9ae8f8e
Mona <monaofficial27@gmail.com>
2022-02-17 14:49:01 -0500

    modal changes

--- script/forms.js
+++ script/forms.js
@@ -10,11 +10,11 @@ function make_demographic_form() {
     var education_options = ["Undergraduate", "Masters", "PhD", "Postdoctoral", "Prefer not to answer"];
     var discipline_value = ["Humanities", "Social Sciences", "Natural Sciences", "Formal Sciences", "Applied Science", "Prefer not to answer"];
     var discipline_labels = [
-        "Humanities (Arts, History, Languages and literature, Law, music, philosophy, religion, theater, journalism)",
-        "Social Sciences (Anthropology, Economics, Geography, Political science, Psychology, Sociology)",
-        "Natural Sciences (Biology, Chemistry, Space science, Earth science, physics)",
-        "Formal Sciences (Computer Science, Mathematics)",
-        "Applied Science (Economics, Engineering and technology, Medicine and health, Agriculture)",
+        "Humanities (Arts, History, Languages and literature, Law, Music, Philosophy, Theater, Journalism..)",
+        "Social Sciences (Anthropology, Economics, Geography, Political science, Psychology, Sociology..)",
+        "Natural Sciences (Biology, physics, Chemistry, Space science, Earth science, Life sciences..)",
+        "Formal Sciences (Computer Science, Mathematics, Statistics, Information science..)",
+        "Applied Science (Business, Engineering & technology, Medicine & health, Architecture, Agriculture..)",
         "Prefer not to answer"
     ]
 
@@ -122,7 +122,7 @@ function make_eng_form() {
                     // value: eng_scale[j - 1],
                     value:j,
                     class: 'form-check-input',
-                    checked: 'true',
+                    // checked: 'true',
                     required: 'true'
                 })
             ).append(
@@ -185,7 +185,7 @@ function make_empathy_form(form, type) {
                     value:j,
                     // value: empathy_scale[j - 1],
                     class: 'form-check-input',
-                    checked: 'checked',
+                    // checked: 'checked',
                     required: 'true'
                 })
             ).append(
